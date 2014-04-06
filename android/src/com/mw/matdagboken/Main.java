1b1743301b2a49c70bb05cdcfa59d8d565ec33be
McF1st <karl.martin.wester@gmail.com>
2014-04-06 12:50:37 +0200

    Added CameraLayout Class
    Tried to get camera working… failed… didn’t get the activity result
    callback for some reason

--- android/src/com/mw/matdagboken/Main.java
+++ android/src/com/mw/matdagboken/Main.java
@@ -1,23 +1,30 @@
 package com.mw.matdagboken;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-//import android.view.Menu;
-//import android.view.MenuItem;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Button;
-import android.widget.RelativeLayout;
-//import android.os.Build;
 
 public class Main extends Activity
 {
+	private CameraLayout mCameraLayout;
+	
+	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-
-        RelativeLayout myLayout = new RelativeLayout(this);
-        myLayout.setBackgroundColor(0xFFFFFFFF);
-        setContentView(myLayout);
+		
+		mCameraLayout = new CameraLayout(this);
+		setContentView(mCameraLayout.getContentView());
+		
+		mCameraLayout.tryStartCameraActivity(this);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{
+	    if (mCameraLayout != null) 
+	    {
+	        mCameraLayout.onActivityResult(requestCode, resultCode, data);
+	    }
 	}
 }
