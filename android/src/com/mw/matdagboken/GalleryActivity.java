d6a09d863b6e0ae4461b7505e17aa85f6a691c3a
toftan <emma.toft@gmail.com>
2014-05-10 13:30:52 +0200

    Added toast, FoodshotActivity, Actionbar features

--- android/src/com/mw/matdagboken/GalleryActivity.java
+++ android/src/com/mw/matdagboken/GalleryActivity.java
@@ -1,17 +1,17 @@
 package com.mw.matdagboken;
 
 import java.util.ArrayList;
-
-import android.app.Activity;
+import android.app.ActionBar;
 import android.content.ClipData.Item;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class GalleryActivity extends Activity implements OnClickListener
+public class GalleryActivity extends FoodshotActivity implements OnClickListener
 {
 	private GridView mGridView; //Why not set to null??
 	//private ArrayList<Item> mGridArray = new ArrayList<Item>(); 
@@ -26,11 +26,30 @@ public class GalleryActivity extends Activity implements OnClickListener
 		GridView gridView = (GridView) findViewById(R.id.galleryGridView);
 		gridView.setAdapter(new ImageAdapter(this));
 		
+		ActionBar actionBar = getActionBar();
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		
 	/*	gridView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 				Toast.makeText(GalleryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
 		    	}
 		});*/
+	}
+	
+	//Respond to actions in the action bar
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) 
+	    {
+	        case android.R.id.home:
+	        	finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 	@Override
